259a6165da940fcd3280319e3a0f570a2d530198
Ruslan Sakal <ruslan.sakal@gmail.com>
2025-07-11 13:17:20 +0200

    added custom logger

---
+++ plugins/logger-backend/src/index.ts
@@ -0,0 +1,1 @@
+export { loggerService as default } from './logger';
---
+++ plugins/logger-backend/src/logger.ts
@@ -0,0 +1,34 @@
+import {
+  coreServices,
+  createServiceFactory,
+} from '@backstage/backend-plugin-api';
+import { WinstonLogger } from '@backstage/backend-defaults/rootLogger';
+import { createConfigSecretEnumerator } from '@backstage/backend-defaults/rootConfig';
+import { transports, format } from 'winston';
+
+export const loggerService = createServiceFactory({
+  service: coreServices.rootLogger,
+  deps: {
+    config: coreServices.rootConfig,
+  },
+
+  async factory({ config }) {
+    const logger = WinstonLogger.create({
+      meta: {
+        service: 'backstage',
+      },
+      level: process.env.LOG_LEVEL || 'info',
+      transports: [new transports.Console()],
+      format:
+        process.env.NODE_ENV === 'production'
+          ? format.json()
+          : WinstonLogger.colorFormat(),
+    });
+
+    const secretEnumerator = await createConfigSecretEnumerator({ logger });
+    logger.addRedactions(secretEnumerator(config));
+    config.subscribe?.(() => logger.addRedactions(secretEnumerator(config)));
+
+    return logger;
+  },
+});
